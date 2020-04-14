c8867647c0da174f2940f36300da289bd1ebe4d2
Aaron <goolyaaron@gmail.com>
2020-04-14 23:38:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,142 +1,46 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
-const request = require('request');
-const client = new Discord.Client();
+const { Client, Collection } = require("discord.js");
+const economy = require("./economy.js");
+
 const config = require("./config.json");
-const music = require("./commands/music/music.js");
-
-
-//attaching the config to the CLIENT so it's accessible everywhere.
-client.config = config;
-
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
+const fs = require("fs");
+const client = new Client({
+    disableEveryone: true
 });
 
-client.commands = new Enmap();
-//loading all commands
-//loading fun
-fs.readdir("./commands/fun/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/fun/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load Fun ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-//loading utility
-fs.readdir("./commands/utility/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/utility/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load Utility ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-//loading moderation
-fs.readdir("./commands/moderation/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/moderation/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load moderation ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-//loading bot owner commands aka eval
-fs.readdir("./commands/botowner/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/botowner/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load botowner ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-//loading 18+ NSFW commands
-fs.readdir("./commands/nsfw/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/nsfw/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load nsfw directory ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-//loading Music Module
-fs.readdir("./commands/music/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/music/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load Music module ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
-//loading Misc Module
-fs.readdir("./commands/misc/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/misc/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load misc module ${commandName}`);
-    client.commands.set(commandName, props);
-  });
+client.commands = new Collection();
+client.aliases = new Collection();
+
+client.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
 });
 
-//loading Radio Module
-fs.readdir("./commands/radio/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/radio/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load radio module ${commandName}`);
-    client.commands.set(commandName, props);
-  });
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+    
+client.user.setActivity('-help', { type: 'WATCHING' });
+
 });
 
+client.on("message", async message => {
+    const prefix = "-";
 
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
 
-//Log the Bot in.
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-client.on('guildMemberAdd', member => {
-  let logChannel = member.guild.channels.find('name', 'welcome');
-  
-    let logEmbed = new Discord.RichEmbed()
-    .setAuthor("New member joined!") 
-    .setDescription("Welcome to the server " + member.user.username.toString())
-    .setColor('RANDOM')
-    .setFooter("enjoy your stay", member.user.displayAvatarURL)
-    .setTimestamp()
-    logChannel.send(logEmbed);
-  })
-  client.on('guildMemberRemove', member => {
-  let logChannel = member.guild.channels.find('name', 'welcome');
-  
-    let logEmbed = new Discord.RichEmbed()
-    .setAuthor("Member left the server") 
-      .setDescription("Goodbye " + member.user.username )
-    .setFooter("sorry to see you leave", member.user.displayAvatarURL)
-    .setColor('RANDOM')
-    .setTimestamp()
-    logChannel.send(logEmbed);
-  })
-  
+    if (command) 
+        command.run(client, message, args);
+});
 
 client.login(config.token);
