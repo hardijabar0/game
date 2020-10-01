25298e214a6e40808d24e5c43a6e5035668f9583
znix <64723880+znixhook@users.noreply.github.com>
2020-10-01 13:54:08 +0530

    Better code + Error handling

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -1,32 +1,28 @@
-    const Discord = require("discord.js")
+const Discord = require("discord.js")
 
-    module.exports = {
+module.exports = {
     name: "avatar",
     category: "utility",
     description: "Gets the avatar of a user or yourself",
     usage: "[command | user] or [command]",
-    run: async (client, message, args) => {
-    //command
-    
-{
-    const user = message.mentions.users.first();
-    if(!user)
-        return message.reply('Please mention the user who you want the avatar from.');
+    run: async(client, message, args) => {
 
-    if(!user.avatarURL)
-        return message.channel.send(`That user does not have an avatar`);
+        //command
 
-    {
-    
-    const avatar = new Discord.MessageEmbed()
-          .setTitle(`${user.username}'s Avatar`)
-          .setColor("RANDOM")
-          .setImage(user.avatarURL())
-          .setURL(user.avatarURL())  
-          message.channel.send(avatar)
-        };
+        /* If user isnt found it selects ur profile */
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
+
+        if (!user.avatarURL) return message.channel.send(`That user does not have an avatar`);
+
+        const avatar = new Discord.MessageEmbed()
+            .setTitle(`${user.username}'s Avatar`)
+            .setColor("RANDOM")
+            .setImage(user.avatarURL())
+            .setURL(user.avatarURL())
+        message.channel.send(avatar)
+            // If bot doesnt have embed perms 
+            .catch(() => message.channel.send('**Error:** Missing permission `Embed link` '));
+
     }
-       
-    }
-    
-    };
+
+};
