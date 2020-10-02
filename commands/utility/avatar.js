f1c124cadacc7a659a41617b4aae2d5c60ebb6f4
znix <64723880+znixhook@users.noreply.github.com>
2020-10-02 13:06:45 +0530

    facepalm

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -10,15 +10,15 @@ module.exports = {
         //command
 
         /* If user isnt found it selects ur profile */
-        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
 
-        if (!user.avatarURL) return message.channel.send(`That user does not have an avatar`);
+        if (!member.user.avatarURL) return message.channel.send(`That user does not have an avatar`);
 
         const avatar = new Discord.MessageEmbed()
-            .setTitle(`${user.username}'s Avatar`)
+            .setTitle(`${member.user.username}'s Avatar`)
             .setColor("RANDOM")
-            .setImage(user.avatarURL())
-            .setURL(user.avatarURL())
+            .setImage(member.user.avatarURL())
+            .setURL(member.user.avatarURL())
         message.channel.send(avatar)
             // If bot doesnt have embed perms 
             .catch(() => message.channel.send('**Error:** Missing permission `Embed link` '));
