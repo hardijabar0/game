21d94f7059a64496638a5b936516f390012aad7c
znix <64723880+znixhook@users.noreply.github.com>
2020-10-02 12:58:41 +0530

    fixed wrong const

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -10,7 +10,7 @@ module.exports = {
         //command
 
         /* If user isnt found it selects ur profile */
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
 
         if (!user.avatarURL) return message.channel.send(`That user does not have an avatar`);
 
